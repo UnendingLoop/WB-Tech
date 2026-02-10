e4b56d0c562148ba922837f7d9dec050ed8fa535
FANIL KAMA <1618396fkam@gmail.com>
2026-02-10 01:20:22 +0000

    Fix: added nil-channel-friendly processing

--- LEVEL2/L2.14/main.go
+++ LEVEL2/L2.14/main.go
@@ -6,41 +6,65 @@ import (
 	"time"
 )
 
-func recursiveOr(channels ...<-chan interface{}) <-chan interface{} {
-	var or func(channels ...<-chan interface{}) <-chan interface{} // если не объявить отдельно, то компилятор не даст возможность рекурсивного вызова
+func recursiveOr(channels ...<-chan any) <-chan any {
+	var or func(channels ...<-chan any) <-chan any // если не объявить отдельно, то компилятор не даст возможность рекурсивного вызова
+	// отсеиваем ниловые каналы
+	filtered := make([]<-chan any, 0)
+	for _, v := range channels {
+		if v == nil {
+			continue
+		}
+		filtered = append(filtered, v)
+	}
 
-	or = func(channels ...<-chan interface{}) <-chan interface{} {
-		switch len(channels) {
+	// запускаем рекурсию
+	or = func(chans ...<-chan any) <-chan any {
+		switch len(chans) {
 		case 0:
 			return nil
 		case 1:
-			return channels[0]
+			return chans[0]
 		}
-		out := make(chan interface{})
+		out := make(chan any)
 		go func() {
 			select {
-			case <-channels[0]:
-			case <-or(channels[1:]...):
+			case <-chans[0]:
+			case <-or(chans[1:]...):
 			}
 			defer close(out)
 		}()
 
 		return out
 	}
-	return or(channels...)
+
+	// возврат 'объединенного' канала
+	return or(filtered...)
 }
 
-func nonRecursiveOr(channels ...chan interface{}) chan interface{} {
-	out := make(chan interface{})
+func nonRecursiveOr(channels ...<-chan any) <-chan any {
+	out := make(chan any)
+
+	// отсеиваем ниловые каналы
+	filtered := make([]<-chan any, 0)
+	for _, v := range channels {
+		if v == nil {
+			continue
+		}
+		filtered = append(filtered, v)
+	}
+
+	// готовим 'одноразовую' функцию
 	closeOnce := sync.Once{}
-	switch len(channels) {
+
+	// запускаем цикл
+	switch len(filtered) {
 	case 0:
 		return nil
 	case 1:
-		return channels[0]
+		return filtered[0]
 	default:
-		for _, v := range channels {
-			go func(ch chan interface{}) {
+		for _, v := range filtered {
+			go func(ch <-chan any) {
 				<-ch
 				closeOnce.Do(func() { close(out) })
 			}(v)
@@ -50,8 +74,8 @@ func nonRecursiveOr(channels ...chan interface{}) chan interface{} {
 }
 
 func main() {
-	sig := func(after time.Duration) chan interface{} {
-		c := make(chan interface{})
+	sig := func(after time.Duration) chan any {
+		c := make(chan any)
 		go func() {
 			defer close(c)
 			time.Sleep(after)
@@ -64,6 +88,7 @@ func main() {
 		sig(5*time.Second),
 		sig(7*time.Second),
 		sig(9*time.Second),
+		nil,
 		sig(11*time.Second),
 		sig(13*time.Second),
 	)
@@ -74,6 +99,7 @@ func main() {
 		sig(5*time.Second),
 		sig(7*time.Second),
 		sig(9*time.Second),
+		nil,
 		sig(11*time.Second),
 		sig(13*time.Second),
 	)
